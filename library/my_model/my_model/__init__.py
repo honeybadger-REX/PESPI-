9ecf915238b07165e6c6f686990cb1ce2dea0335
honeybadger-REX <priyanshbadger257@gmail.com>
2026-04-10 00:37:54 +0530

    library version 1
    this is first  version of  my  project library more  update  and  debugin will  be  done  in futher

---
+++ library/my_model/my_model/__init__.py
@@ -0,0 +1,4 @@
+from .core import vehical_dection_fn,mongo_setUP
+
+
+__all__ = ["vegical_dection_fn", "mongo_setup"]
---
+++ library/my_model/test.py
@@ -0,0 +1,5 @@
+from my_model import mongo_setUP , vehical_dection_fn
+
+
+print("done  with  succes")
+vehical_dection_fn(r"D:\GIT_ml\PESPI\train modles\vehical_detection.pt",r"D:\reseacr dataset\video dataset test\12207144_1920_1080_30fps.mp4","we","tyu",False,False)
